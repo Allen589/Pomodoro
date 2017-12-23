f8e7096b4db4ca57486cbc4cd4a59c99e2526f74
averybadtime <allenliu@Allens-MBP.home>
2017-12-23 00:32:16 -0500

    improved functionality and interface

--- timer.js
+++ timer.js
@@ -1,26 +1,50 @@
+/**
+ * Initially called when extension page loads. Sets up a bunch of stuff.
+ */
 function init() {
-  addMessageListeners();
-  startTimer();
+	addOnClick();
+	addMessageListeners();
+	startTimer();
 }
 
+/**
+ * Sends a message to background page to start the timer.
+ */
 function startTimer() {
-  chrome.runtime.sendMessage({"command": "startTimer"},
-    function(response) {
-      console.log(response);
-    });
+	chrome.runtime.sendMessage({
+		"command": "startTimer"
+	}, function(response) {
+		console.log(response.message);
+	});
 }
 
+/**
+ * Adds listeners so it knows how to handle the messages from the background page.
+ */
 function addMessageListeners() {
-  chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.command === "updateTime") {
-          var time = request.time;
-          document.getElementById("time").innerText = time;
-        }
-    });
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		switch(request.command) {
+			case "updateTime":
+				document.getElementById("current-time").innerText = request.time;
+				break;
+			case "timerEnded":
+				console.log("Timer ended.");
+				break;
+		}
+	});
 }
 
-
-
+/**
+ * Adds onclick listener to the stop button.
+ */
+function addOnClick() {
+	document.getElementById("stop").onclick = function() {
+		chrome.runtime.sendMessage({
+			"command": "endTimer"
+		});
+		document.location = chrome.runtime.getURL("popup.html");
+		chrome.browserAction.setBadgeText({"text" : ""});
+	}
+}
 
 document.addEventListener('DOMContentLoaded', init);
